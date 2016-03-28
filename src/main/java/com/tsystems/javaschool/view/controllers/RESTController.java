fa42f20cc5be400255333c428a4ab30106f38a82
advortsov <alxdvcv@gmail.com>
2016-03-29 02:31:39 +0300

    added pass encryption

--- src/main/java/com/tsystems/javaschool/view/controllers/RESTController.java
+++ src/main/java/com/tsystems/javaschool/view/controllers/RESTController.java
@@ -1,7 +1,5 @@
 package com.tsystems.javaschool.view.controllers;
 
-import com.tsystems.javaschool.dto.BookDTO;
-import com.tsystems.javaschool.dto.ClientDTO;
 import com.tsystems.javaschool.dto.OrderDTO;
 import com.tsystems.javaschool.services.interfaces.AdminManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,24 +28,5 @@ public class RESTController {
         List<OrderDTO> ordersDTOPerPeriod = adminManager.getOrdersDTOPerPeriod(startDate, endDate);
         return ordersDTOPerPeriod;
     }
-
-//    @RequestMapping(value = "/clients", method = RequestMethod.GET, produces = "application/json")
-//    @ResponseBody
-//    public List<ClientDTO>
-//    getClientsPerPeriod(@RequestParam(value = "start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
-//                       @RequestParam(value = "end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate) {
-//        List<ClientDTO> list = adminManager.getClientsDTOPerPeriod(startDate, endDate);
-//        return list;
-//    }
-//
-//    @RequestMapping(value = "/books", method = RequestMethod.GET, produces = "application/json")
-//    @ResponseBody
-//    public List<BookDTO>
-//    getBooksPerPeriod(@RequestParam(value = "start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
-//                       @RequestParam(value = "end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate) {
-//        List<BookDTO> list = adminManager.getBooksDTOPerPeriod(startDate, endDate);
-//        return list;
-//    }
-
 
 }
